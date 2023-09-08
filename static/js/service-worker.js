dc07d4d4dafa1c7608c43180bcd71ab4a09b661a
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-09-08 16:50:18 +0100

    PWA: add manifest and service worker for offline using

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -39,7 +39,7 @@ self.addEventListener('fetch', function(event) {
         return response || fetch(event.request);
       })
       .catch(function() {
-        return caches.match('/offline.html');
+        return caches.match('/static/offline.html');
       })
   );
 });
